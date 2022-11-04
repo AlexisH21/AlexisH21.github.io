ea5fcdeb9895575c85d1ea09825f07bde597a453
Alexis Hays <115569303+AlexisH21@users.noreply.github.com>
2022-11-04 15:33:30 -0500

    Add files via upload

---
+++ GuessingGame/GuessingGame/GuessingGame/Game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Game
+    {
+        int Guess = 0;
+        int Target = 6;
+        string Input = "";
+        string[] Words = { "cat", "hat", "rat", "bat" };
+        Random RandomNumber = new Random();
+        public void Play()
+        {
+            Target = RandomNumber.Next(Words.Length);
+            Console.WriteLine("Can you guess what word I'm thinking of ?!");
+            for (int i = 0; i < Words.Length; i++)
+            {
+                if (i == (Words.Length - 1))
+                    Console.Write("or " + Words[i] + "? ");
+                else
+                    Console.Write(Words[i] + ", ");
+            }
+            Input = Console.ReadLine();
+            if(Input == Words[Target])
+            {
+                Console.WriteLine(" Congrats!! You guessed CORRECT!!");
+               
+            }
+            else
+            {
+                Console.WriteLine(" Oh No! That was incorrect :( \n  Press Enter to continue.");
+                Console.ReadLine();
+                Play();
+            }
+            Console.ReadKey();
+        }
+         
+    }
+}
